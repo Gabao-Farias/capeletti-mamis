c0b68cfc10b9dbdff16b64d07d466f2381c86ee1
Gabriel Taborda Farias <badanhamaster@gmail.com>
2020-07-29 14:04:12 -0300

    FinishedOrder View created

--- projeto/src/view/FinishedOrder/index.js
+++ projeto/src/view/FinishedOrder/index.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 
-import {Component} from './styles';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import {Container, Title, Description, Back, BackText} from './styles';
 
 export default class FinishedOrder extends Component{
     render(){
-        <Container></Container>
+        return(
+            <Container>
+                <Icon name="clipboard-check-outline" size={48} color="#fff"/>
+                <Title>Entregue!</Title>
+                <Description>Pedido adicionado à lista de pedidos concluídos!</Description>
+                <Back onPress={() => {this.props.navigation.goBack()}}>
+                    <BackText>Voltar</BackText>
+                </Back>
+            </Container>
+        );
     }
 }
--- projeto/src/view/FinishedOrder/styles.js
+++ projeto/src/view/FinishedOrder/styles.js
@@ -7,4 +7,33 @@ export const Container = styled(LinearGradient).attrs({
     end: {x: 1, y: 1},
 })`
     flex: 1;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const Title = styled.Text`
+    align-self: center;
+    margin-top: 20px;
+    color: #fff;
+    font-size: 38px;
+`;
+
+export const Description = styled.Text`
+    color: #ddd;
+    font-size: 14px;
+`;
+
+export const Back = styled.TouchableOpacity`
+    position: absolute;
+    bottom: 20px;
+    padding: 10px;
+    border: 1px solid #D85050;
+    border-radius: 12px;
+    width: 120px;
+`;
+
+export const BackText = styled.Text`
+    align-self: center;
+    font-size: 22px;
+    color: #D85050;
 `;
