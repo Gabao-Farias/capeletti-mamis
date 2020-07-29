b287c7266b01460d7c8b7a387f71b769646cc276
Gabriel Taborda Farias <badanhamaster@gmail.com>
2020-07-29 15:24:33 -0300

    Changed route

--- projeto/src/view/FinishedOrder/index.js
+++ projeto/src/view/FinishedOrder/index.js
@@ -11,8 +11,8 @@ export default class FinishedOrder extends Component{
                 <Icon name="clipboard-check-outline" size={48} color="#fff"/>
                 <Title>Entregue!</Title>
                 <Description>Pedido adicionado à lista de pedidos concluídos!</Description>
-                <Back onPress={() => {this.props.navigation.navigate("PendingOrders")}}>
-                    <BackText>Voltar</BackText>
+                <Back onPress={() => {this.props.navigation.navigate("Main")}}>
+                    <BackText>Voltar ao menu</BackText>
                 </Back>
             </Container>
         );
